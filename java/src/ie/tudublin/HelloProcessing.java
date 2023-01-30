2abb525c8847ec53cf336dc045e2124bb51b666a
DanRussuleac <drussuleac@gmail.com>
2023-01-30 12:20:07 +0000

    practice work

--- java/src/ie/tudublin/HelloProcessing.java
+++ java/src/ie/tudublin/HelloProcessing.java
@@ -11,7 +11,7 @@ public class HelloProcessing extends PApplet
 	}
 
 	public void setup() {
-		colorMode(HSB);
+		//colorMode(HSB);
 		background(0);
 
 	
@@ -22,6 +22,19 @@ public class HelloProcessing extends PApplet
 	
 	public void draw()
 	{	
-		
+		stroke(255); //color of line
+		line(10,10,100,100); //x1, y1, x2, y2
+		circle(300,250,70); //first two param(center of circle), third param is diameter
+		fill(60,190,0); //fill gaps between lines
+		rect(10, 300, 10, 100); //tlx, tly, width and length
+		stroke(200);
+		fill(0,140,255);
+		noStroke(); //no color for line
+		noFill(); //no more color filling
+		strokeWeight(DEFAULT_HEIGHT); //thickness of line
+		triangle(40, 90, 300, 20, 80, 70);
+		fill(255,0,255);
+
+
 	}
 }
